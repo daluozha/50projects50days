1dafa52c6b656948b49d3d619550956551dd4256
daluozha <561890199@qq.com>
2022-05-04 19:49:19 +0800

    finish day01-expanding-cards

--- src/components/day01-expanding-cards.vue
+++ src/components/day01-expanding-cards.vue
@@ -1,5 +1,37 @@
-<script>
+<script lang="ts">
 export default {
+  data() {
+    return {
+
+      panelsData: [
+        {
+          id: 1,
+          url: 'https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+          text: 'Explore The World'
+        },
+        {
+          id: 2,
+          url: 'https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+          text: 'Wild Forest'
+        },
+        {
+          id: 3,
+          url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80',
+          text: 'Sunny Beach'
+        },
+        {
+          id: 4,
+          url: 'https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+          text: 'City on Winter'
+        },
+        {
+          id: 5,
+          url: 'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+          text: 'Mountains - Clouds'
+        }
+      ]
+    }
+  },
   mounted() {
     const panels = document.querySelectorAll('.panel')
 
@@ -22,25 +54,9 @@ export default {
 
 <template>
   <div class="container">
-    <div class="panel active"
-      style="background-image: url('https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')">
-      <h3>Explore The World</h3>
-    </div>
-    <div class="panel"
-      style="background-image: url('https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')">
-      <h3>Wild Forest</h3>
-    </div>
-    <div class="panel"
-      style="background-image: url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80')">
-      <h3>Sunny Beach</h3>
-    </div>
-    <div class="panel"
-      style="background-image: url('https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80')">
-      <h3>City on Winter</h3>
-    </div>
-    <div class="panel"
-      style="background-image: url('https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')">
-      <h3>Mountains - Clouds</h3>
+    <div v-for="item in panelsData" :id="item.id" class="panel" :class="item.id == 1 ? 'active' : ''"
+      :style="{ 'background-image': `url(${item.url})` }">
+      <h3>{{ item.text }}</h3>
     </div>
   </div>
 </template>
