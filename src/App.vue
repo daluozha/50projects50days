468e261b82785f8618a01f7bed9051ccc6fd87be
daluozha <561890199@qq.com>
2022-05-04 12:09:13 +0800

    update day01

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
-<script setup>
-import expandingCards from './components/expanding-cards.vue'
+<script setup lang="ts">
+import expandingCards from './components/day01-expanding-cards.vue'
 </script>
 
 <template>
